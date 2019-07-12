f70fc159fcc26d6f28b8b183eb0292125a9e6b05
JungGeon1 <51072422+JungGeon1@users.noreply.github.com>
2019-07-12 18:50:02 +0900

    Update LoginCheckFilter.java

--- FirstWeb/src/filter/LoginCheckFilter.java
+++ FirstWeb/src/filter/LoginCheckFilter.java
@@ -43,14 +43,16 @@ public class LoginCheckFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		// TODO Auto-generated method stub
 		// place your code here
+		//request를 httpservletRequest타입으로 형변환 해준다
 		HttpServletRequest req = (HttpServletRequest)request;
 		HttpServletResponse resp=(HttpServletResponse)response; 
-		
+		//HttpSession이 존재하면 현재 HttpSession을 반환하고 존재하지 않으면 새로이 생성하지 않고 그냥 null을 반환합니다
 		HttpSession session = req.getSession(false);
 		boolean loginChk=false;
-		
+		//세션이 존재하면서 로그인이포가 널이 아니면 로그인 체크를 트루로 해서 필터를 통과
 		if(session!=null &&session.getAttribute("LoginInfo")!=null) {
 			loginChk=true;
+		//로그인체크가 트루면 실행
 		}if(loginChk) {
 			chain.doFilter(request, response);
 		}else {
